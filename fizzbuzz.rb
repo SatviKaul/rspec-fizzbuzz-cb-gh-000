2234de0414c0c89db83663a2bf7c585e79dba2fd
Satvik Kaul <satvik.1799@gmail.com>
2019-08-12 16:05:20 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
   if int % 3 == 0
-    puts "Fizz"
+    print "Fizz"
   elsif int % 5 == 0
-    puts "Buzz"
+    print "Buzz"
   elsif int % 3 == 0 && int % 5 == 0
-    puts "FizzBuzz"
+    print "FizzBuzz"
   else
     return nil
   end
